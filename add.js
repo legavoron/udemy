e847105d5e20251b9f5f845856eb92c0b49f4a9b
AlehShchurko <alehshchurko@gmail.com>
2021-12-18 16:43:47 +0300

    course: part 12

--- add.js
+++ add.js
@@ -1,3 +1,19 @@
-alert('hello');
-console.log('hello');
-const aleh = 'Aleh';
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const lastMovies = prompt('Один из последних просмотренных фильмов');
+const rating = +prompt('На сколько оцените его');
+const lastMovies_2 = prompt('Один из последних просмотренных фильмов');
+const rating_2 = +prompt('На сколько оцените его');
+
+personalMovieDB.movies[lastMovies] = rating;
+personalMovieDB.movies[lastMovies_2] = rating_2;
+console.log(personalMovieDB);
+
